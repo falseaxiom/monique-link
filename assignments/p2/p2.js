b505b722ec71502eb52c99cedf6641c32b49b78f
falseaxiom <mlegaspi@princeton.edu>
2022-03-29 21:26:48 -0400

    persistent tasks

--- assignments/p2/p2.js
+++ assignments/p2/p2.js
@@ -1,5 +1,7 @@
+/***** SETUP *****/
+
 // get event variables, create if they don't exist
-if (!localStorage.getItem("events")) localStorage.setItem('events', '{"02_04":"c&&event&&f__t&&event&&f__s&&event&&f"}');
+if (!localStorage.getItem("events")) localStorage.setItem('events', '{"02_04":"c&&circle&&f__t&&triangle&&f__s&&square&&f"}');
 let events = JSON.parse(localStorage.getItem("events"));
 
 // label kou
@@ -32,33 +34,43 @@ for (let i = 0; i < eKeys.length; i++) {
     let month = date[0];
     let day = date[1];
 
-    let kou = convert(month, day);
+    // get kou number
+    let k = convert(month, day);
 
     let val = events[key];
     let evs = val.split("__");
-    console.log(evs)
     for (let j = 0; j < evs.length; j++) {
         let info = evs[j].split("&&");
-        makeEvent(kou, info);
+        let evid = key + "?" + j;
+        makeEvent(k, evid, info);
     }
 }
 
+/***** FUNCTIONS *****/
+
+// convert date to kou
+function convert(month, day) {
+    return 1;
+}
+
 // make event div
-function makeEvent(k, info) {
-    // create event div
+function makeEvent(k, evid, info) {
+    // create event div, tag key+j as id
     let ev = document.createElement("div");
     ev.classList.add("event");
+    ev.id = evid;
     ev.onclick = function() {check(this)};
 
     let done = info[2] == "t"
 
     // append bullet to event
+    let type = info[0];
     let b = document.createElement("span");
     b.classList.add("bullet");
-    b.classList.add(info[0]);
-    if      (info[0] == "c") b.innerHTML = done ? "●" : "○";
-    else if (info[0] == "t") b.innerHTML = done ? "▲" : "△";
-    else                     b.innerHTML = done ? "■" : "□";
+    b.classList.add(type);
+    if      (type == "c") b.innerHTML = done ? "●" : "○";
+    else if (type == "t") b.innerHTML = done ? "▲" : "△";
+    else                  b.innerHTML = done ? "■" : "□";
     ev.appendChild(b);
 
     // append description to event
@@ -78,9 +90,9 @@ function makeEvent(k, info) {
     }
 }
 
-// convert date to kou
-function convert(month, day) {
-    return 1;
+// add event to list
+function addEvent() {
+    
 }
 
 // check off events
@@ -89,7 +101,22 @@ function check(div) {
     let event = div.firstChild.nextSibling;
     let checked = event.style.textDecoration == "line-through";
     event.style.textDecoration = checked ? "none" : "line-through";
-    if      (bullet.classList.contains("c"))   bullet.innerHTML = checked ? "○" : "●";
-    else if (bullet.classList.contains("t")) bullet.innerHTML = checked ? "△" : "▲";
-    else if (bullet.classList.contains("s"))   bullet.innerHTML = checked ? "□" : "■";
+
+    let type;
+    if      (bullet.classList.contains("c")) { bullet.innerHTML = checked ? "○" : "●"; type = "c"; }
+    else if (bullet.classList.contains("t")) { bullet.innerHTML = checked ? "△" : "▲"; type = "t"; }
+    else if (bullet.classList.contains("s")) { bullet.innerHTML = checked ? "□" : "■"; type = "s"; }
+
+    // make sure to update localStorage!
+    let evid = div.id.split("?");
+    events = JSON.parse(localStorage.getItem("events"));
+    let ev = events[evid[0]].split("__");
+    ev[evid[1]] = type + "&&" + event.innerHTML + "&&" + (checked ? "f" : "t");
+    let evstr = "";
+    for (let i = 0; i < ev.length - 1; i++) {
+        evstr += ev[i] + "__";
+    }
+    evstr += ev[ev.length-1];
+    events[evid[0]] = evstr;
+    localStorage.setItem("events", JSON.stringify(events));
 }
